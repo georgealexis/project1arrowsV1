70ad97207a8bf0685a13f5144d5a0f20a402953f
George Alexis <igeorgealexix@gmail.com>
2022-11-07 13:29:12 +0800

    with animations

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import $ from "jquery";
 import "./style.css";
+import "animate.css";
 
 //? global variables
 const arrowDirection = ["left", "up", "down", "right"];
@@ -31,80 +32,85 @@ const getRandomInt = (numberofdirectionarrows) => {
 const createArrow = () => {
   let x = arrowDirection[getRandomInt(4)];
   const $arrow = $("<i>").addClass(
-    `fa fa-arrow-circle-${x} arrow animated bounceIn`
+    `fa fa-arrow-circle-${x} arrow animate__animated animate__bounceIn`
   );
   $(".gameBody").append($arrow);
   return (directionOfArrow = "Arrow" + `${x}`);
 };
 
+// https://animate.style/
 // x will return the direction of the arrow
 // create arrow and append it to the gamebody
 // return arrow+(direction of arrow) - to be use later
 
 $("#start").on("click", () => {
-  createArrow();
-  $("#start").hide();
-
-  // when start button is clicked
-  // createArrow function is called
-  // start button is hidden
-
-  const timerBarFrame = () => {
-    if (timerBarWidthNumerator <= 0) {
-      clearInterval(timerBarInterval);
-      $("body").off("keydown");
-      //https://api.jquery.com/unbind/
-    }
-
-    if (currentscore > highscore) {
-      highscore = currentscore;
-      $("#highscore").text(`${highscore}`);
-    }
-
-    //! to insert code to disable any inputs when timer = 0 (done)
-    //! compare current score with highscore (done)
-    //! if current score greater than highscore save current score as highscore (done)
-    //* https://www.geeksforgeeks.org/how-to-disable-a-button-in-jquery-dialog-from-a-function/
-    else {
-      timerBarWidthNumerator -= 10;
-      const currentTimerBarWidth =
-        timerBarWidthNumerator / timerBarWidthDenominator;
-      $("#timerBar").css("width", `${currentTimerBarWidth * 100}%`);
-    }
-  };
-
-  let timerBarWidthNumerator = gameDuration * 1000;
-  const timerBarWidthDenominator = gameDuration * 1000;
-  const timerBarInterval = setInterval(timerBarFrame, 10);
-
-  /* Checking if the arrow key pressed is the same as the arrow on screen. If it is, it will increase the
-current score by 1, remove the arrow on screen and create a new arrow. If it is not, it will decrease the current score by 1. */
-
-  $("body").on("keydown", (event) => {
-    let a = directionOfArrow.toLowerCase();
-    let b = event.code.toLowerCase();
-    console.log(a);
-    console.log(b);
-    if (a === b) {
-      currentscore++;
-      $("#currentscore").text(`${currentscore}`);
-      $("main").css("color", "green");
-      $(".arrow").remove();
-      createArrow();
+  function countdown(time) {
+    if (time > 0) {
+      $("h1").text(`${time}`);
+      setTimeout(function () {
+        countdown(time - 1);
+      }, 1000);
     } else {
-      currentscore--;
-      $("#currentscore").text(`${currentscore}`);
-    }
-  });
+      $("h1").text(`ARROW!`);
+      createArrow();
+      $("#start").hide();
 
-  //! to insert keydown functions for all 4 directions (???)
-  //? when clicking the arrow, remove the current one before calling the next arrow
+      //? when the start button is clicked, it will run the createarrow function and hide the start button
+      //? timer to start only after the start button is clicked
+      const timerBarFrame = () => {
+        if (timerBarWidthNumerator <= 0) {
+          $("h1").text("GAMEOVER");
+          clearInterval(timerBarInterval);
+          $("body").off("keydown");
+          //* https://api.jquery.com/unbind/
+          //? code to disable any inputs when timer = 0 (done)
+        }
+        //? compare current score with highscore (done)
+        if (currentscore > highscore) {
+          highscore = currentscore;
+          $("#highscore").text(`${highscore}`);
+        }
+
+        //? if current score greater than highscore save current score as highscore (done)
+
+        //* https://www.geeksforgeeks.org/how-to-disable-a-button-in-jquery-dialog-from-a-function/
+        else {
+          timerBarWidthNumerator -= 10;
+          const currentTimerBarWidth =
+            timerBarWidthNumerator / timerBarWidthDenominator;
+          $("#timerBar").css("width", `${currentTimerBarWidth * 100}%`);
+        }
+      };
+
+      let timerBarWidthNumerator = gameDuration * 1000;
+      const timerBarWidthDenominator = gameDuration * 1000;
+
+      //? Calling the timerBarFrame function every 10ms.
+      const timerBarInterval = setInterval(timerBarFrame, 10);
+
+      //? if the arrow key pressed is the same as the arrow on screen. If it is, it will increase the current score by 1, remove the arrow on screen and create a new arrow. If it is not, it will decrease the current score by 1. */
+
+      $("body").on("keydown", (event) => {
+        let a = directionOfArrow.toLowerCase();
+        let b = event.code.toLowerCase();
+        //? changing both string to lowercase for comparison
+        if (a === b) {
+          currentscore++;
+          $("#currentscore").text(`${currentscore}`);
+          $(".arrow").remove();
+          createArrow();
+          //? when clicking the arrow, remove the current one before calling the next arrow
+        } else {
+          currentscore--;
+          $("#currentscore").text(`${currentscore}`);
+        }
+      }); // replace with any function
+    }
+  }
+  countdown(3);
 });
 
-//? when the start button is clicked, it will run the first arrow function and hide the start button
-//! to get the timer to start only after the start button is clicked (??)
-
-//*https://stackoverflow.com/questions/15420558/jquery-click-event-not-working-after-append-method
+// https://stackoverflow.com/questions/15420558/jquery-click-event-not-working-after-append-method
 // $(".gameBody").on("click", ".arrow", () => {
 //   currentscore++;
 //   $("#currentscore").text(`${currentscore}`);
@@ -113,11 +119,12 @@ current score by 1, remove the arrow on screen and create a new arrow. If it is 
 // });
 
 $("#reset").on("click", () => {
+  $("h1").text("A-R-R-O-W");
   currentscore = 0;
   $("#currentscore").text("0");
   $(".arrow").remove();
   $("#start").show();
-  $("#timerBar").css("width", "100%");
+  // $("#timerBar").css("width", "100%");
 });
 
 // when 'play again' button is clicked, start button appears

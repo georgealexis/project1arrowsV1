5e1e7c7066c3edf03eee9a176961e840e12b1bdc
George Alexis <igeorgealexix@gmail.com>
2022-11-07 11:32:13 +0800

    working without animations

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const arrowDirection = ["left", "up", "down", "right"];
 let currentscore = 0;
 let highscore = 0;
 const gameDuration = 5;
+let directionOfArrow = "";
 
 const $mainbody = $("<main>").addClass("gameBody");
 $("body").append($mainbody);
@@ -28,32 +29,43 @@ const getRandomInt = (numberofdirectionarrows) => {
 //? firstArrow function where it creates the first arrow from the random number generated and append it to the main body of the game
 
 const createArrow = () => {
-  const $arrow = $("<i id=("Arrow"+ "(`${arrowDirection[getRandomInt(4)]}`>)"))".addClass(
-    `fa fa-arrow-circle-${arrowDirection[getRandomInt(4)]} arrow`
+  let x = arrowDirection[getRandomInt(4)];
+  const $arrow = $("<i>").addClass(
+    `fa fa-arrow-circle-${x} arrow animated bounceIn`
   );
   $(".gameBody").append($arrow);
+  return (directionOfArrow = "Arrow" + `${x}`);
 };
 
-//! to create next arrow (done)
+// x will return the direction of the arrow
+// create arrow and append it to the gamebody
+// return arrow+(direction of arrow) - to be use later
 
 $("#start").on("click", () => {
   createArrow();
   $("#start").hide();
 
+  // when start button is clicked
+  // createArrow function is called
+  // start button is hidden
+
   const timerBarFrame = () => {
     if (timerBarWidthNumerator <= 0) {
       clearInterval(timerBarInterval);
-      $("body").prop("disabled", true);
-      //! $(".arrow").prop("disabled", true);
-      if (currentscore > highscore) {
-        highscore = currentscore;
-        $("#highscore").text(`${highscore}`);
-      }
-      //! to insert code to disable any inputs when timer = 0 (done)
-      //! compare current score with highscore (done)
-      //! if current score greater than highscore save current score as highscore (done)
-      //* https://www.geeksforgeeks.org/how-to-disable-a-button-in-jquery-dialog-from-a-function/
-    } else {
+      $("body").off("keydown");
+      //https://api.jquery.com/unbind/
+    }
+
+    if (currentscore > highscore) {
+      highscore = currentscore;
+      $("#highscore").text(`${highscore}`);
+    }
+
+    //! to insert code to disable any inputs when timer = 0 (done)
+    //! compare current score with highscore (done)
+    //! if current score greater than highscore save current score as highscore (done)
+    //* https://www.geeksforgeeks.org/how-to-disable-a-button-in-jquery-dialog-from-a-function/
+    else {
       timerBarWidthNumerator -= 10;
       const currentTimerBarWidth =
         timerBarWidthNumerator / timerBarWidthDenominator;
@@ -65,20 +77,25 @@ $("#start").on("click", () => {
   const timerBarWidthDenominator = gameDuration * 1000;
   const timerBarInterval = setInterval(timerBarFrame, 10);
 
-/* Checking if the arrow key pressed is the same as the arrow on screen. If it is, it will increase the
-current score by 1, remove the arrow on screen and create a new arrow. If it is not, it will
-decrease the current score by 1. */
-  // $("body").on("keydown", (event) => {
-  //   if(arrow key pressed = arrow on screen){
-  //     currentscore++;
-  //     $("#currentscore").text(`${currentscore}`);
-  //     $(".arrow").remove();
-  //     createArrow();
-  //   } else {
-  //     currentscore--;
-  //     $("#currentscore").text(`${currentscore}`);
-  //   }
-  // });
+  /* Checking if the arrow key pressed is the same as the arrow on screen. If it is, it will increase the
+current score by 1, remove the arrow on screen and create a new arrow. If it is not, it will decrease the current score by 1. */
+
+  $("body").on("keydown", (event) => {
+    let a = directionOfArrow.toLowerCase();
+    let b = event.code.toLowerCase();
+    console.log(a);
+    console.log(b);
+    if (a === b) {
+      currentscore++;
+      $("#currentscore").text(`${currentscore}`);
+      $("main").css("color", "green");
+      $(".arrow").remove();
+      createArrow();
+    } else {
+      currentscore--;
+      $("#currentscore").text(`${currentscore}`);
+    }
+  });
 
   //! to insert keydown functions for all 4 directions (???)
   //? when clicking the arrow, remove the current one before calling the next arrow
